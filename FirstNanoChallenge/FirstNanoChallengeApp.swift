e146cdb95f516f5bf262fccbdcc1b4b698180fee
profit0124 <31499563+profit0124@users.noreply.github.com>
2022-04-27 16:11:36 +0900

    Initial Commit

---
+++ FirstNanoChallenge/FirstNanoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstNanoChallengeApp.swift
+//  FirstNanoChallenge
+//
+//  Created by Sooik Kim on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct FirstNanoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstNanoChallengeUITests/FirstNanoChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FirstNanoChallengeUITestsLaunchTests.swift
+//  FirstNanoChallengeUITests
+//
+//  Created by Sooik Kim on 2022/04/27.
+//
+
+import XCTest
+
+class FirstNanoChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
